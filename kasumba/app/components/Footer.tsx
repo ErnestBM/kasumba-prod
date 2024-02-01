7565de00fccbd584a303926e6315ee2e32f10fd7
abbymarvel <abbymarvel97@gmail.com>
2024-02-01 23:59:38 +0700

    feat: footer (to be finished)

---
+++ kasumba/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <p className='h1'>This is Footer</p>
+    </div>
+  )
+}
+
+export default Footer
--- kasumba/app/menu/page.tsx
+++ kasumba/app/menu/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-import Link from 'next/link';
 import Image from 'next/image';
 import menuData from './kasumbasSpecial.json';
 import { useState } from 'react';
+import Footer from '../components/Footer';
 
 
 export default function Menu() {
@@ -33,20 +33,19 @@ export default function Menu() {
         </div>
         <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
           <p className="relative left-0 right-0 mx-auto mt-5 max-w-xl text-center text-xl  font-semibold uppercase tracking-wide text-orange-600">
-            The Coffee House
+            Kasumba Coffee
           </p>
           <h1 className="mt-1 text-center font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-7xl">
             <span className="block text-white">Life is better with</span>
             <span className="block text-orange-500">coffee</span>
           </h1>
-
-          <div className="mx-auto mt-10 max-w-xs sm:flex sm:max-w-none sm:justify-center">
+          {/* <div className="mx-auto mt-10 max-w-xs sm:flex sm:max-w-none sm:justify-center">
             <button
               className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-orange-600 shadow-sm hover:bg-orange-100 sm:px-8"
               onClick={() => handleCategoryChange('')}>
               Shop coffees
             </button>
-          </div>
+          </div> */}
         </div>
       </div>
       
@@ -90,6 +89,7 @@ export default function Menu() {
           ))}
         </div>
       </div>
+      <Footer/>
     </body>
   )
 }
